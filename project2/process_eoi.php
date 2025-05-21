0ff1be021380121ee76a20fe7b560b022b2da9f3
LucasJurgec <105923018@student.swin.edu.au>
2025-05-21 23:24:26 +1000

    get inputs simple

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -13,4 +13,24 @@ if (!$conn ) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+function clean_input($data) {
+    return htmlspecialchars(stripslashes(trim($data)));
+}
+
+$jobid = clean_input($_POST['jobid']);
+$firstname = clean_input($_POST['firstname']);
+$lastname = clean_input($_POST['lastname']);
+$dob = clean_input($_POST['dob']);
+$gender = isset($_POST['gender']) ? clean_input($_POST['gender']) : '';
+
+$street = clean_input($_POST['street']);
+$suburb = clean_input($_POST['suburb']);
+$postcode = clean_input($_POST['postcode']);
+$state = clean_input($_POST['state'])
+
+$phone = clean_input($_POST['phone']);
+$email = clean_input($_POST['email']);
+
+$otherskills = clean_input($_POST['otherskills']);
+
 ?>
