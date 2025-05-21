e647cfd0ba4556bf0ab3d84b78085f51b7ec806e
TwentyTwoCharacters <105912430@student.swin.edu.au>
2025-05-21 14:22:45 +1000

    manage page protection - Luke

--- project2/staff/manage.php
+++ project2/staff/manage.php
@@ -1,4 +1,9 @@
 <?php 
-require_once("settings.php");
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: .\staff\staff_login.php");
+    exit;
+}
+require_once("./settings.php");
 
 ?>
