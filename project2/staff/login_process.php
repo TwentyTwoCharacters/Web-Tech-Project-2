be0c4e0b45c9315a10904962c59876219dce3f7c
TwentyTwoCharacters <105912430@student.swin.edu.au>
2025-05-21 14:17:34 +1000

    Task 5 Login Testing - luke

--- project2/staff/login_process.php
+++ project2/staff/login_process.php
@@ -5,7 +5,7 @@ $password=$_POST['password'];
 
 if ($username == 'admin' && $password == 'password123'){
     $_SESSION['user'] = $username;
-    header('Location: welcome.php');
+    header('Location: .\staff\manage.php');
 } else {
     echo "Invalid login. <a href='./staff/staff_login.php'> Try again</a>";
 }
