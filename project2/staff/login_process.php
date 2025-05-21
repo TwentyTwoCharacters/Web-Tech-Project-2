6830222a54d725a348a1ac3cc184753f3233a900
TwentyTwoCharacters <105912430@student.swin.edu.au>
2025-05-21 14:19:04 +1000

    bvcvhjhgv - luke

--- project2/staff/login_process.php
+++ project2/staff/login_process.php
@@ -5,7 +5,7 @@ $password=$_POST['password'];
 
 if ($username == 'admin' && $password == 'password123'){
     $_SESSION['user'] = $username;
-    header('Location: .\staff\manage.php');
+    header('Location:.\manage.php');
 } else {
     echo "Invalid login. <a href='./staff/staff_login.php'> Try again</a>";
 }
