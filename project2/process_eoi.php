39402636ae3eede095f1a1b157b268d73f305da2
LucasJurgec <105923018@student.swin.edu.au>
2025-05-21 23:15:03 +1000

    $conn added

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -7,4 +7,10 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
 
 require_once("settings.php");
 
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+if (!$conn ) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 ?>
