8ba03a32458a6c55687ce2658426deba11fb858f
TwentyTwoCharacters <105912430@student.swin.edu.au>
2025-05-21 15:00:13 +1000

    Header fix - Luke

--- project2/staff/manage.php
+++ project2/staff/manage.php
@@ -9,9 +9,16 @@ require_once("../settings.php");
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title> Manager Portal</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="About us">
+    <meta name="keywords" content="HTML5">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="styles\styles.css">
+    <title>About Page</title>
 </head>
 
 <?php   include '../include_files/header.inc'; ?>
