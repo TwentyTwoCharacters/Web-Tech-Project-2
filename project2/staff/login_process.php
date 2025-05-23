97a9fef38472dfdc8ccff92701aa6a391d440354
TwentyTwoCharacters <105912430@student.swin.edu.au>
2025-05-23 11:43:50 +1000

    Login Attempt script - Luke

--- project2/staff/login_process.php
+++ project2/staff/login_process.php
@@ -1,12 +1,33 @@
 <?php
 session_start();
+
+
+//set up login attempt system
+if (!isset($_SESSION['failed_attempts'])) {
+    $_SESSION['failed_attempts'] = 0;
+}
+
+
 $username=$_POST['username'];
 $password=$_POST['password'];
 
-if ($username == 'admin' && $password == 'password123'){
-    $_SESSION['user'] = $username;
-    header('Location:.\manage.php');
-} else {
-    echo "Invalid login. <a href='./staff/staff_login.php'> Try again</a>";
+//validation to reach the 3 login attempt limit 
+if ($_SESSION['failed_attempts'] >= 3) {
+    echo "Too many failed attempts. You are locked out.";
+    exit();
 }
-?>
+
+if ($username == 'admin' && $password == 'password123') {
+    $_SESSION['user'] = $username;
+
+    $_SESSION['failed_attempts'] = 0;// < login attempt reset on successful login 
+    
+    header('Location: ./manage.php');
+    exit();
+} else {
+    $_SESSION['failed_attempts']++;
+    echo "Invalid login. Attempt {$_SESSION['failed_attempts']} of 3. ";
+    echo "<a href='./login.php'>Try again</a>";
+}
+
+
--- project2/staff/staff_login.php
+++ project2/staff/staff_login.php
@@ -15,5 +15,17 @@
     </label><br><br>
     <input type="submit" value="Login">
   </form>
+
+  <form method="post" action="login/php">
+  <input type="submit" name="end_session" value="(Debug) end session"><br><br>
+  </form>
+
 </body>
 </html>
+
+<?php
+if (isset($_POST['end_session'])){
+session_start();
+session_destroy();
+}
+?>
